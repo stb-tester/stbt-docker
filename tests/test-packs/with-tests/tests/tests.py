879eb41c9a7555f02546e7f9e2c274be7b97bd8b
David Röthlisberger <david@stb-tester.com>
2018-06-21 11:53:02 +0100

    Add support for Stb-tester v29 (#16)

---
+++ tests/test-packs/with-tests/tests/tests.py
@@ -0,0 +1,11 @@
+import stbt
+import stbt.audio
+
+
+def test_match():
+    assert stbt.match("videotestsrc-redblue.png")
+
+
+def test_audio_apis():
+    print stbt.audio.get_rms_volume()
+    stbt.audio.wait_for_volume_change()
